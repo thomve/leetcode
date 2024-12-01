a68b32c4a7df81d25fce4a386fbf49ee63448253
thomve <thomas.veran@gmail.com>
2024-12-01 21:38:11 +0100

    alg for jump in arrays

--- array_and_strings.cpp
+++ array_and_strings.cpp
@@ -14,8 +14,29 @@ int maxProfit(vector<int>& prices) {
     return max_profit;
 }
 
+bool canJump(vector<int>& nums) {
+    int max_reach = 0;
+    for (size_t i = 0; i < nums.size(); ++i) {
+        if (i > max_reach) {
+            return false;
+        }
+        max_reach = std::max(max_reach, static_cast<int>(i) + nums[i]);
+        if (max_reach >= nums.size() - 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
     std::vector<int> prices = {7, 1, 5, 3, 6, 4};
     std::cout << "Maximum Profit: " << maxProfit(prices) << std::endl;
+
+    std::vector<int> nums = {2, 3, 1, 1, 4};
+    std::cout << (canJump(nums) ? "true" : "false") << std::endl;
+
+    nums = {3, 2, 1, 0, 4};
+    std::cout << (canJump(nums) ? "true" : "false") << std::endl;
+    
     return 0;
 }
