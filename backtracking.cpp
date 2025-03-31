a9c8db57e261a1a4731590a0c6f5a2b960b640d3
thomve <thomas.veran@gmail.com>
2025-03-31 21:10:49 +0200

    added n queens problem with backtracking algorithm

--- backtracking.cpp
+++ backtracking.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
+// Return the answer in any order.
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+// Example:
+// Input: digits = "23"
+// ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 class LetterCombinationsOfAPhoneNumber
 {
 public:
@@ -44,6 +50,34 @@ private:
     }
 };
 
+// The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+// Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+class NQueensSolution
+{
+public:
+    int totalNQueens(int n)
+    {
+        vector<bool> col(n), diag(2 * n - 1), anti_diag(2 * n - 1);
+        return solve(col, diag, anti_diag, 0);
+    }
+
+private:
+    int solve(vector<bool> &col, vector<bool> &diag, vector<bool> &anti_diag, int row)
+    {
+        int n = size(col), count = 0;
+        if (row == n)
+            return 1;
+        for (int column = 0; column < n; column++)
+            if (!col[column] && !diag[row + column] && !anti_diag[row - column + n - 1])
+            {
+                col[column] = diag[row + column] = anti_diag[row - column + n - 1] = true;
+                count += solve(col, diag, anti_diag, row + 1);
+                col[column] = diag[row + column] = anti_diag[row - column + n - 1] = false;
+            }
+        return count;
+    }
+};
+
 int main()
 {
     LetterCombinationsOfAPhoneNumber lc;
@@ -59,6 +93,11 @@ int main()
         cout << comb << " ";
     }
     cout << endl;
+
+    int numberOfQueens = 4;
+    cout << "Enter the value of n: ";
+    NQueensSolution nqs;
+    cout << "Number of solutions: " << nqs.totalNQueens(numberOfQueens) << endl;
 
     return 0;
 }
