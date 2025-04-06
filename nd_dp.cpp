3c9d858f667d60003271bd79346f69bdb530b099
thomve <thomas.veran@gmail.com>
2025-04-06 22:33:37 +0200

    added the multidimensional DP solution for Min Path Sum

--- nd_dp.cpp
+++ nd_dp.cpp
@@ -193,7 +193,30 @@ string longestPalindrome(string s)
 
 // Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
 // Note: You can only move either down or right at any point in time.
-int minPathSum(vector<vector<int>>& grid) {
+// First solution using multidimensional DP: O(mxn)
+int minPathSumWithNdDp(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+
+    dp[0][0] = grid[0][0];
+
+    for (int j = 1; j < n; ++j)
+        dp[0][j] = dp[0][j - 1] + grid[0][j];
+
+    for (int i = 1; i < m; ++i)
+        dp[i][0] = dp[i - 1][0] + grid[i][0];
+
+    for (int i = 1; i < m; ++i) {
+        for (int j = 1; j < n; ++j) {
+            dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[m - 1][n - 1];
+}
+
+// Second solution using 1D DP: O(m)
+int minPathSumWith1dDp(vector<vector<int>>& grid) {
     int m = grid.size(), n = grid[0].size();
     vector<int> dp(n, 0);
 
