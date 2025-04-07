067b05ed8b19234ec8b0ddf96b6382f428c50597
thomve <thomas.veran@gmail.com>
2025-04-07 21:37:09 +0200

    two sum algorithm with hashmap

--- hashmap.cpp
+++ hashmap.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
@@ -24,8 +25,30 @@ int longestConsecutive(vector<int> &nums)
     return longest;
 }
 
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    unordered_map<int, int> num_map;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int complement = target - nums[i];
+        if (num_map.find(complement) != num_map.end())
+        {
+            return {num_map[complement], i};
+        }
+        num_map[nums[i]] = i;
+    }
+    return {};
+}
+
 int main()
 {
     vector<int> nums = {100, 4, 200, 1, 3, 2};
     cout << "Longest Consecutive Sequence Length: " << longestConsecutive(nums) << endl;
+
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    cout << "Indices: [" << result[0] << ", " << result[1] << "]" << endl;
+
+    return 0;
 }
