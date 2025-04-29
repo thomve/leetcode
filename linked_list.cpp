0c7aebea9a1d7e811960fc15b0d6148bb00eafce
thomve <thomas.veran@gmail.com>
2025-04-29 19:45:07 +0200

    Linked list implementation of a LRU cache

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_map>
+#include <list>
 
 using namespace std;
 
@@ -45,7 +47,7 @@ ListNode *rotateRight(ListNode *head, int k)
 }
 
 // Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
-// k is a positive integer and is less than or equal to the length of the linked list. 
+// k is a positive integer and is less than or equal to the length of the linked list.
 // If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
 // You may not alter the values in the list's nodes, only nodes themselves may be changed.
 ListNode *reverseKGroup(ListNode *head, int k)
@@ -81,3 +83,53 @@ ListNode *reverseKGroup(ListNode *head, int k)
 
     return dummy->next;
 }
+
+class LRUCache
+{
+private:
+    int capacity;
+    list<pair<int, int>> cacheList;
+    unordered_map<int, list<pair<int, int>>::iterator> cacheMap;
+
+    void moveToFront(int key)
+    {
+        auto it = cacheMap[key];
+        cacheList.splice(cacheList.begin(), cacheList, it);
+    }
+
+public:
+    LRUCache(int capacity)
+    {
+        this->capacity = capacity;
+    }
+
+    int get(int key)
+    {
+        if (cacheMap.find(key) == cacheMap.end())
+        {
+            return -1;
+        }
+        moveToFront(key);
+        return cacheMap[key]->second;
+    }
+
+    void put(int key, int value)
+    {
+        if (cacheMap.find(key) != cacheMap.end())
+        {
+            cacheMap[key]->second = value;
+            moveToFront(key);
+        }
+        else
+        {
+            if (cacheList.size() == capacity)
+            {
+                auto lru = cacheList.back();
+                cacheMap.erase(lru.first);
+                cacheList.pop_back();
+            }
+            cacheList.emplace_front(key, value);
+            cacheMap[key] = cacheList.begin();
+        }
+    }
+};
