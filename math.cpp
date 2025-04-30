c85556856c264a4c3fca39fa531cd3f252af147e
thomve <thomas.veran@gmail.com>
2025-04-30 20:09:00 +0200

    raw implementation of raw function

--- math.cpp
+++ math.cpp
@@ -36,3 +36,22 @@ int maxPoints(vector<vector<int>>& points) {
     
     return ans + 1;
 }
+
+// Implement pow(x, n), which calculates x raised to the power n (i.e., x^n).
+double myPow(double x, int n) {
+    long long N = n;
+    if (N < 0) {
+        x = 1 / x;
+        N = -N;
+    }
+    
+    double result = 1.0;
+    while (N) {
+        if (N % 2 == 1) {
+            result *= x;
+        }
+        x *= x;
+        N /= 2;
+    }
+    return result;
+}
