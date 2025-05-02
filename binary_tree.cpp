c67273af1bece751bc0942239ed8270428667820
thomve <thomas.veran@gmail.com>
2025-05-02 20:37:29 +0200

    lowest common ancestor

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -90,3 +90,17 @@ public:
         return buildtree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
     }
 };
+
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (root == nullptr || root == p || root == q)
+        return root;
+
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if (left != nullptr && right != nullptr)
+        return root;
+
+    return left != nullptr ? left : right;
+}
